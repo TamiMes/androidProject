193bd5438c3754fec7c6a89f59d1b895be1c26da
Tami Mesengiser <TamiM@feed-u.co>
2025-02-24 01:37:35 +0200

    store items

--- app/src/main/java/com/example/androidproject_tamara_hen/data/myData.java
+++ app/src/main/java/com/example/androidproject_tamara_hen/data/myData.java
@@ -4,26 +4,59 @@ import com.example.androidproject_tamara_hen.R;
 
 public class myData {
 
-    public static String[] nameArray = {"The wand from deathly hollows", "The wand of Lord Voldemort", "Blessing card", "Metal Captain America Shield"
-            ,"Mike Ehrmantraut", "Saul Goodman", "Skyler White"
-            ,"Walter White JR", "Nacho Varga", "Lalo Salamanca"};
+    public static String[] nameArray = {"The wand from deathly hollows", "The wand of Lord Voldemort", "Non Edible Chocolate Frog", "Dobby  Figure"
+            ,"Invisibility Cloak", "Philosopher’s Stone", "The Golden Snitch"
+            ," Hermione's Time Turner", "Tom Riddle’s Diary ", "Marvolo Gaunt's Ring","Slytherin's Locket","The Hufflepuff Cup","The Sorting Hat"};
     public static String[] versionArray = {
             "The Elder Wand, one of the Deathly Hallows, is the most powerful wand in the wizarding world. Made from elder wood with a Thestral tail hair core, it is said to be unbeatable in duels. According to legend, it was created by Death himself!"
             ,
             "Lord Voldemort's wand is 13½ inches long, made of yew wood, with a phoenix feather core. It was crafted by Garrick Ollivander and shares a unique connection with Harry Potter’s wand, as both have cores from the same phoenix—Fawkes, Dumbledore’s companion.  "
 
-            , "Inspired by the heartwarming moment from The Simpsons ,the card Raphael gave to Lisa, this card of love and the beauty of life’s simple blessings. "
-
-            , "100% Handmade: This is a high-quality handmade round shield."
+            ,"Officially authorized by Warner Brothers.Squash it, stretch it, squish it, or just admire it on your desk."
 
 
-            , "5", "6", "7", "8", "9","10"};
 
-    public static Integer[] drawableArray = {R.drawable.wanddeathlyhollows, R.drawable.voldermortwandpng, R.drawable.trainblessing,
-            R.drawable.sheild, R.drawable.deadpool_funko_pop, R.drawable.deadpool_funko_pop,
-            R.drawable.deadpool_funko_pop, R.drawable.deadpool_funko_pop, R.drawable.deadpool_funko_pop,R.drawable.deadpool_funko_pop};
+            , "The beloved character of the harry potter world,the loyal elf,dont forget to give him your suck!"
 
-    public static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            , " The cloak, one of the Deathly Hallows, allows the wearer to vanish from sight, providing protection and secrecy. It was passed down to Harry Potter from his father, James"
+
+            , " A legendary magical object created by Nicolas Flamel. It grants immortality "
+
+            , " Every Seeker dreams of catching the Golden Snitch, and now you can own a piece of Quidditch history! This beautifully crafted, winged treasure isn’t just a collector’s item—it’s a symbol."
+
+            , "A magical device that allows the wearer to travel back in time. Hermione used one in Prisoner of Azkaban to attend multiple classes."
+
+            ,  " A Horcrux that contained a piece of Voldemort’s soul, capable of manipulating and possessing those who wrote in it"
+
+
+            , "A Horcrux that passed through the male Gaunts, generation to generation,[2] until it was stolen from Morfin Gaunt by Tom Riddle,"
+
+            , "The Slytherin locket was shown to put dark thoughts into the wearer's mind"
+
+            , "A golden cup belonging to Helga Hufflepuff, hidden in Bellatrix Lestrange’s vault."
+
+            , "Harry Potter: \"Not Slytherin, not Slytherin.\"\n" +
+            "Sorting Hat: \"Not Slytherin, eh? Are you sure? You could be great, you know, it’s all here in your head, and Slytherin will help you on the way to greatness,"
+
+            , "14"
+
+            ,  "15"
+
+
+            , "16"
+
+
+
+
+
+    };
+//price of stone:$169.99
+    public static Integer[] drawableArray = {R.drawable.wanddeathlyhollows, R.drawable.voldermortwandpng, R.drawable.chocolatefrog,
+            R.drawable.dobby, R.drawable.cloack, R.drawable.philostone,
+            R.drawable.goldensnich, R.drawable.turntime, R.drawable.riddlediary,R.drawable.marvolo,R.drawable.locket,R.drawable.helga,R.drawable.sortinghat};
+
+    public static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15};
 
     public static Integer[] price = {
             1,
