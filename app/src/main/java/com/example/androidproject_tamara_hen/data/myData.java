60bd13fba924cf24573b4db435370a0f8ba45085
Tami Mesengiser <TamiM@feed-u.co>
2025-02-24 11:25:15 +0200

    store items

--- app/src/main/java/com/example/androidproject_tamara_hen/data/myData.java
+++ app/src/main/java/com/example/androidproject_tamara_hen/data/myData.java
@@ -6,7 +6,7 @@ public class myData {
 
     public static String[] nameArray = {"The wand from deathly hollows", "The wand of Lord Voldemort", "Non Edible Chocolate Frog", "Dobby  Figure"
             ,"Invisibility Cloak", "Philosopher’s Stone", "The Golden Snitch"
-            ," Hermione's Time Turner", "Tom Riddle’s Diary ", "Marvolo Gaunt's Ring","Slytherin's Locket","The Hufflepuff Cup","The Sorting Hat"};
+            ," Hermione's Time Turner", "Tom Riddle’s Diary ", "Marvolo Gaunt's Ring","Slytherin's Locket","The Hufflepuff Cup","The Sorting Hat","Nimbus 2000","The Half-Blood Prince’s Potions Book ","Potter Butterbeer Mug","The Mirror of Erised","Marauder's Map ","Hogwarts School Trunk","Luna Lovegood Spectrespecs","Hogwarts Acceptance Letter"};
     public static String[] versionArray = {
             "The Elder Wand, one of the Deathly Hallows, is the most powerful wand in the wizarding world. Made from elder wood with a Thestral tail hair core, it is said to be unbeatable in duels. According to legend, it was created by Death himself!"
             ,
@@ -39,35 +39,65 @@ public class myData {
             , "Harry Potter: \"Not Slytherin, not Slytherin.\"\n" +
             "Sorting Hat: \"Not Slytherin, eh? Are you sure? You could be great, you know, it’s all here in your head, and Slytherin will help you on the way to greatness,"
 
-            , "14"
+            , "\"The Nimbus 2000: Harry’s First Ride into the Wizarding World! ✨\"\n" +
+            "\n" +
+            "Swift, sleek, and legendary—Harry Potter’s first broomstick, gifted by Professor McGonagall, wasn’t just any broom. It was the key to his first Quidditch match"
 
-            ,  "15"
+            ,  "\"The Half-Blood Prince’s Potions Book – Power, Knowledge, and Secrets Within! ✨\"\n" +
+            "\n" +
+            "Tattered, ink-stained, and filled with brilliant spell modifications, this isn’t just any potions book—it’s the key to mastering magic beyond the classroom. Once belonging to the mysterious Half-Blood Prince"
 
 
-            , "16"
+            , "\"Raise a Toast with a Butterbeer Mug! ✨\"\n" +
+            "\n" +
+            "Step into the wizarding world and enjoy the sweet, frothy taste of Butterbeer just like Harry, Hermione, and Ron at The Three Broomsticks!\n 16 fluid ounce(473.17 ml)"
 
+            ,"A magical mirror that shows a person’s deepest desires, but can trap people in their dreams,like harry saw his parents \n 16.5in (42cm) "
+
+            ,"A magical map showing every secret passage and the real-time location of everyone in Hogwarts."
+
+            ,"This Hogwarts School Trunk has been crafted to resemble those used by Hogwarts students for travel on the Hogwarts Express. \n" +
+            "\n" +
+            "This decorative collectable Harry Potter trunk is sure to delight any recipient! The Hogwarts Trunk measures 45cm x 30cm x 23cm "
+
+            ,"As worn by Luna Lovegood in Harry Potter and the Half-Blood Prince. These replica Spectrespecs are made of hard plastic, with a printed pattern across the frame. One lens is pink, and the other is blue. "
+             , "\"Your Hogwarts Journey Begins Here!   \n" +
+            "\n" +
+            "\"We are pleased to inform you that you have been accepted at Hogwarts School of Witchcraft and Wizardry.\""
 
 
 
 
     };
-//price of stone:$169.99
+
     public static Integer[] drawableArray = {R.drawable.wanddeathlyhollows, R.drawable.voldermortwandpng, R.drawable.chocolatefrog,
             R.drawable.dobby, R.drawable.cloack, R.drawable.philostone,
-            R.drawable.goldensnich, R.drawable.turntime, R.drawable.riddlediary,R.drawable.marvolo,R.drawable.locket,R.drawable.helga,R.drawable.sortinghat};
+            R.drawable.goldensnich, R.drawable.turntime, R.drawable.riddlediary,R.drawable.marvolo,R.drawable.locket,R.drawable.helga,R.drawable.sortinghat,R.drawable.nimbus2000,R.drawable.halfblood,R.drawable.butterbeer,R.drawable.mirror,R.drawable.map,R.drawable.suitcase,R.drawable.letter};
 
-    public static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15};
+    public static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19};
 
-    public static Integer[] price = {
-            1,
-            2,
-            3,
-            4,
-            5,
-            6,
-            7,
-            8,
-            9,
-            10,
+    public static double[] price = {
+            18.99,
+            16.99,
+            10.59,
+            10.59,
+            15.99,
+            169.99,
+            19.99,
+            56.79,
+            33.99,
+            67.99,
+            56.79,
+            89.99,
+            43.17,
+            566.95,
+            20.95,
+            34.99,
+            188.99,
+            40.56,
+            93.06,
+            20.99,
+            34.99
+
     } ;
 }
